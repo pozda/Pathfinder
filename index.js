f2301f8c43d63d32c0022698b12e173dc16d29dc
Ivan Pozderac <ivan.pozderac@gmail.com>
2023-12-15 00:54:33 +0100

    a lot of logic added, need to add and fix some cases

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ class Pathfinder {
     compareArrays = (arrayOne, arrayTwo) => JSON.stringify(arrayOne) === JSON.stringify(arrayTwo)
 
     // methods
-    findStartAndFinishPosition = (array, glyph) => {
+    findStartAndFinishPosition(array, glyph) {
         const rowResults = [] 
         const position = []
 
@@ -99,37 +99,37 @@ class Pathfinder {
         }
     }
 
-    getNewPositionData = (direction) => { 
+    getNewPositionData(direction) { 
         //returns new position data, other method decides which data it will use 
         switch(direction) {
         case directions.UP:
             return ({
                 position: [this.currentPosition[0] - 1, this.currentPosition[1]],
                 direction: directions.UP,
-                glyph: this.matrix[this.position[0] - 1][this.position[1]]
+                glyph: this.matrix[this.currentPosition[0] - 1][this.currentPosition[1]]
             })
         case directions.RIGHT:
             return ({
                 position: [this.currentPosition[0], this.currentPosition[1] + 1],
                 direction: directions.RIGHT,
-                glyph: this.matrix[this.position[0]][this.position[1] + 1]
+                glyph: this.matrix[this.currentPosition[0]][this.currentPosition[1] + 1]
             })
         case directions.DOWN:
             return ({
                 position: [this.currentPosition[0] + 1, this.currentPosition[1]],
                 direction: directions.DOWN,
-                glyph: this.matrix[this.position[0] + 1][this.position[1]]
+                glyph: this.matrix[this.currentPosition[0] + 1][this.currentPosition[1]]
             })
         case directions.LEFT:
             return ({
                 position: [this.currentPosition[0], this.currentPosition[1] - 1],
                 direction: directions.LEFT,
-                glyph: this.matrix[this.position[0]][this.position[1] - 1]
+                glyph: this.matrix[this.currentPosition[0]][this.currentPosition[1] - 1]
             })
         }
     }
 
-    updateVariables = (position) => {
+    updateVariables(position){
         this.currentPosition = position
         this.path.push(position)
         this.currentGlyph = this.matrix[position[0]][position[1]]
@@ -139,100 +139,304 @@ class Pathfinder {
         }
     }
 
-    setDirection = () => {
+    setDirection() {
         const glyphsCollected = []
-        const directions = []
+        const directionChanges = []
+        const directionObjects = []
 
         if (this.currentPosition[0] !== 0) { //all but first row
-            // console.log('loop 1, checks up')
-            const glyphUp = getGlyph(position, directions.UP)
+            const glyphUp = this.getNewPositionData(directions.UP)
+            directionObjects.push(glyphUp)
             if(this.currentDirection !== directions.DOWN){
-                if(this.isValidGlyph(glyphUp)) {
-                    glyphsCollected.push(glyphUp)
-                    directions.push(directions.UP)
+                if(this.isValidGlyph(glyphUp.glyph)) {
+                    glyphsCollected.push(glyphUp.glyph)
+                    directionChanges.push(directions.UP)
                     this.currentDirection = directions.UP
                 }
             }
         }  
-        if(position[0] !== matrix.length-1) { //all but last row
-            // console.log('loop 2, checks down')
-            const glyphDown = getGlyph(position, direction.DOWN)
-            if(this.currentDirection !== direction.UP) {
-                if(isValidGlyph(glyphDown)) {
-                    glyphsCollected.push(glyphDown)
-                    directions.push(direction.DOWN)
-                    this.currentDirection = direction.DOWN
+        if(this.currentPosition[0] !== this.matrix.length-1) { //all but last row
+            const glyphDown = this.getNewPositionData(directions.DOWN)
+            directionObjects.push(glyphDown)
+            if(this.currentDirection !== directions.UP) {
+                if(this.isValidGlyph(glyphDown.glyph)) {
+                    glyphsCollected.push(glyphDown.glyph)
+                    directionChanges.push(directions.DOWN)
+                    this.currentDirection = directions.DOWN
                 }
             }
         }
-        if(position[1] !== matrix[position[0]].length-1) { // all but last column) 
-            // console.log('loop 3, checks right')
-            const glyphRight = getGlyph(position, direction.RIGHT)
-            if(this.currentDirection !== direction.LEFT) {
-                if(isValidGlyph(glyphRight)) {
-                    glyphsCollected.push(glyphRight)
-                    directions.push(direction.RIGHT)
-                    this.currentDirection = direction.RIGHT
+        if(this.currentPosition[1] !== this.matrix[this.currentPosition[0]].length-1) { // all but last column) 
+            const glyphRight = this.getNewPositionData(directions.RIGHT)
+            directionObjects.push(glyphRight)
+            if(this.currentDirection !== directions.LEFT) {
+                if(this.isValidGlyph(glyphRight.glyph)) {
+                    glyphsCollected.push(glyphRight.glyph)
+                    directionChanges.push(directions.RIGHT)
+                    this.currentDirection = directions.RIGHT
                 }
             }
         }
-        if (position[1] !== 0) { //all but first column
-            // console.log('loop 4, checks left')
-            const glyphLeft = getGlyph(position, direction.LEFT)
-            if(this.currentDirection !== direction.RIGHT) {
-                if(isValidGlyph(glyphLeft)) {
-                    glyphsCollected.push(glyphLeft)
-                    directions.push(direction.LEFT)
-                    this.currentDirection = direction.LEFT
+        if (this.currentPosition[1] !== 0) { //all but first column
+            const glyphLeft = this.getNewPositionData(directions.LEFT)
+            directionObjects.push(glyphLeft)
+            if(this.currentDirection !== directions.RIGHT) {
+                if(this.isValidGlyph(glyphLeft.glyph)) {
+                    glyphsCollected.push(glyphLeft.glyph)
+                    directionChanges.push(directions.LEFT)
+                    this.currentDirection = directions.LEFT
                 }
             }
         }
+
+
+        if(directionChanges.length === 1){
+            this.currentDirection = directionChanges[0]
+            // will need to double check for fake turns or such
+            return this.currentDirection
+        } else if(directionChanges.length === 2) {
+            const newDirection = directionObjects.filter(obj => obj.direction !== this.currentDirection)
+            this.currentDirection = newDirection[0].direction
+            return this.currentDirection
+        } else if(directionChanges.length > 2) {
+            return this.currentDirection
+        }       
+
+    
     }
 
-    walk = (position) => {
+    getResult(){
+        const finishedPath = this.path
+        let path = []
+        for(let i = 0; i < finishedPath.length; i++){
+            path.push(this.matrix[finishedPath[i][0]][finishedPath[i][1]])
+        }
+        const letterArr = Array.from(new Set(this.letterCoordinates.map(JSON.stringify)), JSON.parse)
+        let word = []
+        for(let i = 0; i < letterArr.length; i++){
+            word.push(this.matrix[letterArr[i][0]][letterArr[i][1]])
+        }
+        path = path.join('')
+        word = word.join('')
+        console.log({path, word})
+        return {path, word}
+    }
+
+    walk(position) {
         this.updateVariables(position)
 
         if(this.currentGlyph === glyphs.START){
-            console.log('should setDirection')
+            const firstDirection = this.setDirection()
+            const newPosition = this.getNewPositionData(firstDirection)
+            return this.walk(newPosition.position)
         }
 
         if(this.isValidLetter(this.currentGlyph)){
-            console.log('should check surrounding glyphs')
+            const newDirection = this.setDirection()
+            const newPosition = this.getNewPositionData(newDirection)
+            return this.walk(newPosition.position)
         }
 
         if(this.currentGlyph === glyphs.TURN){
-            console.log('should change direction')
+            const newDirection = this.setDirection()
+            const newPosition = this.getNewPositionData(newDirection)
+            return this.walk(newPosition.position)
         }
 
         if(this.currentGlyph === glyphs.END){
-            console.log('found path')
+            return this.getResult()
         }
         
-        const newPosition = this.findNextPosition()
-        this.walk(newPosition)
+        const newPosition = this.getNewPositionData(this.currentDirection)
+        return this.walk(newPosition.position)
     }
 
-    findPath = () => {
+    findPath() {
         try {
-            // walk(startPosition, endPosition)
-            return this.findStartAndFinishPosition(this.matrix, glyphs.START)
+            return this.walk(this.startPosition)
         } catch (error) {
             console.log(error)
         }
     }
 }
 
+//matrices
 const matrix1 = [
+    ['@', '-', '-', '-', 'A', '-', '-', '-', '+'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+    ['x', '-', 'B', '-', '+', ' ', ' ', ' ', 'C'],
+    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'],
+    [' ', ' ', ' ', ' ', '+', '-', '-', '-', '+']
+] //pass
+const matrix2 = [
+    [' ', ' ', '@'], 
+    [' ', ' ', '|', ' ', '+', '-', 'C', '-', '-', '+'], 
+    [' ', ' ', 'A', ' ', '|', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', '+', '-', '-', '-', 'B', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', 'x'], 
+    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', '+', '-', '-', '-', 'D', '-', '-', '+']
+] //pass
+const matrix3 = [
+    ['@', '-', '-', '-', 'A', '-', '-', '-', '+'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    ['x', '-', 'B', '-', '+', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', '+', '-', '-', '-', 'C']
+] //pass
+const matrix4 = [
+    [' ', ' ', ' ', ' ', ' ', '+', '-', 'O', '-', 'N', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '+', '-', 'I', '-', '+'], 
+    [' ', '@', '-', 'G', '-', 'O', '-', '+', ' ', '|', ' ', '|', ' ', '|'], 
+    [' ', ' ', ' ', ' ', ' ', '|', ' ', '|', ' ', '+', '-', '+', ' ', 'E'], 
+    [' ', ' ', ' ', ' ', ' ', '+', '-', '+', ' ', ' ', ' ', ' ', ' ', 'S'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x']
+] //pass
+const matrix5 = [
+    [' ', '+', '-', 'L', '-', '+'], 
+    [' ', '|', ' ', ' ', '+', 'A', '-', '+'],
+    ['@', 'B', '+', ' ', '+', '+', ' ', 'H'], 
+    [' ', '+', '+', ' ', ' ', ' ', ' ', 'x']
+] //pass
+const matrix6 = [
+    ['@', '-', 'A', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', ' ', '+', '-', 'B', '-', '-', 'x', '-', 'C', '-', '-', 'D']
+] // pass
+//invalid matrices that should produce errors
+const matrix7 =[
+    [' ', ' ', ' ', '-', 'A', '-', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    ['x', '-', 'B', '-', '+', ' ', ' ', ' ', 'C'], 
+    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', '+', '-', '-', '-', '+']
+] // pass
+const matrix8 =
+[
+    ['@', '-', '-', 'A', '-', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', 'B', '-', '+', ' ', ' ', ' ', 'C'], 
+    [' ', ' ', ' ', '|', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', '+', '-', '-', '-', '+']
+] // pass
+const matrix9 = 
+[
+    [' ', '@', '-', '-', 'A', '-', '@', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    ['x', '-', 'B', '-', '+', ' ', ' ', ' ', 'C'], 
+    [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', '+', '-', '-', '-', '+']
+] // pass
+const matrixa =
+[
+    ['@', '-', '-', 'A', '-', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'C'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', '@', '-', 'B', '-', '+']
+] // pass, becase it errors on double starting character
+const matrixab =
+[
+    ['@', '-', '-', 'A', '-', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'C'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', 'B', '-', '+']
+] // pass, becase it errors on double starting character, fails if just 1
+const matrixb =
+[
+    [' ', '@', '-', '-', 'A', '-', '-', 'x'], 
+    [' '], 
+    ['x', '-', 'B', '-', '+'], 
+    [' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', ' ', '@']
+] // pass because double starting and ending characters, but also when because of broken link
+const matrixc =
+[
+    [' ', ' ', ' ', ' ', ' ', 'x', '-', 'B'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    ['@', '-', '-', 'A', '-', '-', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'], 
+    [' ', ' ', 'x', '+', ' ', ' ', ' ', 'C'], 
+    [' ', ' ', ' ', '|', ' ', ' ', ' ', '|'], 
+    [' ', ' ', ' ', '+', '-', '-', '-', '+']
+] // pass because double ending characters, but also pass if just one ending character
+const matrixd =
+[
+    ['@', '-', '-', 'A', '-', '+'], 
+    [' ', ' ', ' ', ' ', ' ', '|'], 
+    [' '], 
+    [' ', ' ', ' ', ' ', ' ', 'B', '-', 'x']
+] // pass
+const matrixe = 
+[
+    ['x', '-', 'B', '-', '@', '-', 'A', '-', 'x']
+] // pass
+const matrixf = 
+[
+    ['@', '-', 'A', '-', '+', '-', 'B', '-', 'x']
+] // pass
+const matrixg = [
+    [' ','x','-','+'],
+    [' ',' ',' ','|'],
+    [' ',' ',' ','A'],
+    [' ',' ',' ','|'],
+    [' ',' ',' ','J'],
+    [' ',' ',' ','|'],
+    [' ',' ',' ','+'],
+    [' ',' ',' ','|'],
+    [' ',' ',' ','@'],
+] // pass
+const matrixTest = [
     [' ', '@', '-', '-', '+'],
     [' ', ' ', ' ', ' ', '|', ' ', 'x', '+'],
     [' ', ' ', ' ', ' ', 'A', '-', 'B', '-'],
 ]
+const matrixTest2 = [
+    [' ','x','-','+'],
+    [' ',' ',' ','|'],
+    [' ',' ',' ','A'],
+    [' ',' ',' ','|'],
+    [' ',' ',' ','|'],
+    ['+','-','-','J'],
+    ['|',' ',' ',' '],
+    ['|',' ',' ',' '],
+    ['+','-','-','@'],
+] //pass
 
 try {
-    const pathfinder = new Pathfinder(matrix1)
+    const pathfinder = new Pathfinder( matrix2 )
     const findPath = pathfinder.findPath()
-    console.log(findPath)
 } catch (error) {
     console.log(error)
 }
 
+
+/**
+ * fails
+ * mtx2
+ * mtx4
+ * mtx5
+ * mtxab - broken path - need to check this one, it fails
+ * mtxc2 - should try to invalidate fork situation if evereything else is ok
+ * mtxd - broken path, goes through
+ * mtxf - fake turn
+ */
+
+
+
+
+// /** matrices that should Error/FAIL
+//  * mtx7: no start character
+//  * mtx8: no end character
+//  * mtx9: 2 start characters
+//  * mtxa: broken path
+//  * mtxab: broken path - need to check this one, it fails
+//  * mtxb: 2 paths - double start and end characters
+//  * mtxc: Fork and two ends
+//  * mtxd: broken path - it powers through, should fail
+//  * mtxe: 2 end characters, start character in the middle
+//  * mtxf: fake TURN, goes through, need to add this one
+//  */
